49c93f84725ec24dfa186cad17ac2dd23bed4a4b
Maksim Perunkov <nanellod@yandex.ru>
2026-01-19 09:02:02 +0300

    fix test_bun.py

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,19 +1,25 @@
 import pytest
 from praktikum.bun import Bun
 
-BUN_DATA = [
-    ("Булочка с кунжутом", 15),
-    ("Мягкая булочка", 20),
-    ("Бриошь", 300)
+BUN_NAMES = [
+    "Булочка с кунжутом",
+    "brioche-XL",
+    "bun_with_underscores_123",
+    "!@#-special-name",
 ]
 
+BUN_PRICES = [0, 1.5, 300, 9999]
+
+
 class TestBun:
-    @pytest.mark.parametrize("name, price", BUN_DATA)
-    def test_get_name_returns_correct_name(self, name, price):
-        bun = Bun(name, price)
+    @pytest.mark.parametrize("name", BUN_NAMES)
+    def test_get_name_returns_correct_name(self, name):
+        bun = Bun(name, price=42)
+
         assert bun.get_name() == name
 
-    @pytest.mark.parametrize("name, price", BUN_DATA)
-    def test_get_price_returns_correct_price(self, name, price):
-        bun = Bun(name, price)
+    @pytest.mark.parametrize("price", BUN_PRICES)
+    def test_get_price_returns_correct_price(self, price):
+        bun = Bun(name="Фиксированное имя", price=price)
+
         assert bun.get_price() == price
